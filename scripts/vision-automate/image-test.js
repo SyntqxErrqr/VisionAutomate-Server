9066a9cb5204429561b0e47c9e8c65eda6539c0a
SyntqxErrqr <boomergame101@gmail.com>
2023-03-03 14:12:30 -0700

    Extra IO.outs in image test

--- scripts/vision-automate/image-test.js
+++ scripts/vision-automate/image-test.js
@@ -3,12 +3,14 @@ module.exports = (config, local, backendAPI) => {
    const IO = new VisionAutomateIO(startScript, { local: local, api: backendAPI });
 
    function startScript() {
+      IO.out("Direct:");
       IO.out("https://imgs.search.brave.com/_3zTXZYcrMHL3JJ5ODocGIfkqcf8iNnlkScHwfLV2_E/rs:fit:711:225:1/g:ce/aHR0cHM6Ly90c2Uy/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC5i/WlJ1YS1HVHp3NGh2/VzFBT2RSY25RSGFF/OCZwaWQ9QXBp", { image: true });
 
       imageFromConfig();
    }
 
    function imageFromConfig() {
+      IO.out("Config:");
       IO.out(config.image, { image: true });
 
       imageFromInput();
@@ -16,6 +18,7 @@ module.exports = (config, local, backendAPI) => {
 
    function imageFromInput() {
       IO.in((input) => {
+         IO.out("Input:");
          IO.out(input, { image: true });
 
          completeScript();
